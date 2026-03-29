d70027b8b27877490f2fd7e54b05a4e86bbdda3c
nader-eftekharmanavi <nadereftekharmanavi@gmail.com>
2026-03-29 20:36:23 +0200

    fix gradio deploy

--- app.py
+++ app.py
@@ -74,4 +74,5 @@ demo = gr.ChatInterface(
 )
 
 if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=7860)
+    port = int(os.environ.get("PORT", 7860))
+    demo.launch(server_name="0.0.0.0", server_port=port)
